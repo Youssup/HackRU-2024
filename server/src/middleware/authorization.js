776c7b10ea5a4f631f0a9a96e495e3b591d0ae83
CJ <iron768gamer@gmail.com>
2024-03-23 12:19:22 -0400

    Add authorization middleware

---
+++ server/src/middleware/authorization.js
@@ -0,0 +1,32 @@
+const modelUser = require("../model/user");
+
+module.exports = {
+    async parseAuthorizationToken(request, response, next) {
+        const authHeader = request.headers.authorization;
+        const token = authHeader?.split(' ')[1];
+
+        if (!token) {
+          return next();
+        }
+
+        const payload = await usersModel.verifyJWT(token);
+
+        request.user = payload;
+
+        // continue
+        next();
+    },
+
+    requireUser(){
+        return function(request, response, next) {
+          if (!request.user) {
+            return next({
+              status: 401,
+              message: 'You need to be logged in!'
+            });
+          }
+
+          next();
+        }
+    }
+}
