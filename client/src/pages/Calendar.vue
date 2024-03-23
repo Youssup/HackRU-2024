108f075b614810df7b584da4fdded511694e113c
youssup <Youssupsong@gmail.com>
2024-03-23 16:55:58 -0400

    Added FullCalendar Library

--- client/src/pages/Calendar.vue
+++ client/src/pages/Calendar.vue
@@ -1,13 +1,32 @@
-<script setup lang="ts">
+<script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth',
+        dateClick: this.handleDateClick,
+        events: [
+          { title: 'event 1', date: '2019-04-01' },
+          { title: 'event 2', date: '2019-04-02' }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleDateClick: function(arg) {
+      alert('date click! ' + arg.dateStr)
+    }
+  }
+}
 </script>
-
 <template>
-    <div>
-
-    </div>
+  <FullCalendar :options="calendarOptions" />
 </template>
-
-<style scoped>
-
-</style>
