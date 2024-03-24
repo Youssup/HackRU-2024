41477fab98aa0367bff303e1e5b5fdfa1e56bcc2
Katrina Tong <ktongk2@gmail.com>
2024-03-23 22:15:49 -0400

    Added suggetions page

--- client/typed-router.d.ts
+++ client/typed-router.d.ts
@@ -20,5 +20,6 @@ declare module 'vue-router/auto-routes' {
   export interface RouteNamedMap {
     '/Calendar': RouteRecordInfo<'/Calendar', '/Calendar', Record<never, never>, Record<never, never>>,
     '/Home': RouteRecordInfo<'/Home', '/Home', Record<never, never>, Record<never, never>>,
+    '/Suggetions': RouteRecordInfo<'/Suggetions', '/Suggetions', Record<never, never>, Record<never, never>>,
   }
 }
